1718edcd9c9307e89743045f8354a6e5f0c08290
Zhu Quan <qzhu@helixnt.com.cn>
2016-07-27 10:35:40 +0800

    fix unround image clip bug

--- customimageview/src/main/java/com/example/customimageview/CornerImageView.java
+++ customimageview/src/main/java/com/example/customimageview/CornerImageView.java
@@ -12,7 +12,6 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ImageView;
 
 /**
@@ -39,7 +38,8 @@ public class CornerImageView extends ImageView{
             resId = a.getResourceId(R.styleable.CornerImage_cornerdrawable,0);
             circleColor = a.getColor(R.styleable.CornerImage_circlecolor,Color.BLUE);
             circlicSize = a.getDimensionPixelSize(R.styleable.CornerImage_cornersize,5);
-        Log.d("circleSize",String.valueOf(circlicSize));
+            hascirclic = a.getBoolean(R.styleable.CornerImage_hascirclic,true);
+            setCircleFactor(a.getFloat(R.styleable.CornerImage_circlefactor,ROUNDFACTOR));
         a.recycle();
     }
 
@@ -66,30 +66,70 @@ public class CornerImageView extends ImageView{
         paint.setAntiAlias(true);
         Bitmap target = Bitmap.createBitmap(min, min, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(target);
+        canvas.drawCircle(min / 2, min / 2, (min / 2)*circleFactor, paint);
 
-        canvas.drawCircle(min / 2, min / 2, min / 2, paint);
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(bitmap, 0 , 0, paint);
+        canvas.drawBitmap(bitmap, (imageWidth-min)/2 , -(imageHeight-min)/2, paint);
 
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
-        paint.setColor(circleColor);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(circlicSize);
-        canvas.drawCircle(min / 2 , min/ 2 , (min-circlicSize) / 2, paint);
+        if(hascirclic)
+        {
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
+            paint.setColor(circleColor);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(circlicSize);
+            canvas.drawCircle(min / 2 , min/ 2 , (min*circleFactor/2-circlicSize/2), paint);
+        }
 
         idrawable = new BitmapDrawable(getResources(),target);
         return idrawable;
     }
 
+    public final static float RECTFACTOR = (float)Math.sqrt(2);
+    public final static float ROUNDFACTOR  = 1f;
+
     private int circleColor = Color.BLUE;
     private int circlicSize = 8;
 
-    public void setCircleColor(int circleColor) {
-        this.circleColor = circleColor;
+    //whether has circlic
+    private boolean hascirclic = true;
+
+    //circle factor 0.5(round)~1(rect)
+    private float circleFactor = ROUNDFACTOR;
+
+    /**
+     * set the circleFcctor between {@link CornerImageView#ROUNDFACTOR} ~ {@link CornerImageView#RECTFACTOR}
+     * @param circleFactor
+     */
+    public void setCircleFactor(float circleFactor) {
+        if(circleFactor <= ROUNDFACTOR)
+        {
+            this.circleFactor = ROUNDFACTOR;
+        }else{
+            this.circleFactor = (circleFactor > RECTFACTOR) ? RECTFACTOR : circleFactor;
+            setHasCirclic(false);
+        }
     }
 
-    public void setCirclicSize(int circlicSize) {
+    public CornerImageView setCircleColor(int circleColor) {
+        this.circleColor = circleColor;
+        return this;
+    }
+
+    public CornerImageView setCirclicSize(int circlicSize) {
         this.circlicSize = circlicSize;
+        return this;
+    }
+
+    public CornerImageView setHasCirclic(boolean hascirclic) {
+        this.hascirclic = hascirclic;
+        return this;
+    }
+
+    public void apply(){
+        if(resId != 0)
+        {
+            setDrawable(createCircleImage(resId));
+        }
     }
 
     @Override
